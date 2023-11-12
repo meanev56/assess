0fc66cc2e92ad45dbfaebe318b673a9a2b89e191
meanev56 <emekanjoku86@gmail.com>
2023-11-12 14:14:27 +0100

    assess

---
+++ src/components/HomePage.jsx
@@ -0,0 +1,131 @@
+import React from 'react'
+import NavbarMenu from '../sections/NavbarMenu'
+
+const HomePage = () => {
+  return (
+    <div>
+      <NavbarMenu />
+      <div className="style__KPIBannerContainer-sc-2a5839c1-0 jdZcjQ">
+  <div className="style__FullWidthContainer-sc-2a5839c1-1 huYVK css-0">
+    <div className="style__StyledContainer-sc-2a5839c1-2 imMgqj css-0">
+      <div className="style__SubscribeBannerContainer-sc-2a5839c1-7 jxSbiB css-0">
+        <div className="style__SubscribeForm-sc-2a5839c1-8 iIHzBj">
+          <h1
+            className="dist__StyledText-sc-630f0a6f-12 bdywXm style__CtaHeader-sc-2a5839c1-10 knpxgB"
+            fontWeight="bold"
+          >
+            Become smarter in just 5 minutes
+          </h1>
+          <p className="dist__StyledText-sc-630f0a6f-12 dOpeuC style__StyledText-sc-2a5839c1-11 jhMjHF">
+            Get the daily email that makes reading the news enjoyable. Stay
+            informed and entertained, for free.
+          </p>
+          <form className="dist__BaseForm-1-sc-630f0a6f-30 dist__StyledSquaredForm-1-sc-630f0a6f-38 hwjhDI kklAbF style__StyledInputWithAction-sc-2a5839c1-9 juSsIh">
+            <input
+              placeholder="youremail@domain.com"
+              type="email"
+              name="email"
+              className="dist__BaseInput-sc-630f0a6f-27 dist__StyledInput-sc-630f0a6f-28 dist__StyledSquaredInput-1-sc-630f0a6f-39 iqqrLh ddGvFv jDJMSE"
+              defaultValue=""
+            />
+            <button type="submit" className="chakra-button css-u5fi7s">
+              Subscribe
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div className="css-1qwj5yi">
+    <a
+      className="chakra-link css-1j8oszi"
+      href="https://www.youtube.com/watch?v=33BbDZTpbrk"
+    >
+      <div className="style__SpotlightImage-sc-11272830-1 eiCAYT css-0">
+        <span
+          style={{
+            boxSizing: "border-box",
+            display: "block",
+            overflow: "hidden",
+            width: "initial",
+            height: "initial",
+            background: "none",
+            opacity: 1,
+            border: 0,
+            margin: 0,
+            padding: 0,
+            position: "relative"
+          }}
+        >
+          <span
+            style={{
+              boxSizing: "border-box",
+              display: "block",
+              width: "initial",
+              height: "initial",
+              background: "none",
+              opacity: 1,
+              border: 0,
+              margin: 0,
+              padding: "52.1429% 0px 0px"
+            }}
+          ></span>
+          <img
+            alt='Bossy podcast hosts surrounding "Therapist vs. Executive Coach?!" text'
+            src="https://cdn.sanity.io/images/bl383u0v/production/89c5baf11a6a85e961321b4c4e539af947f135aa-1280x720.png?rect=0,24,1280,670&w=420&h=220&q=80&fit=crop&auto=format"
+            decoding="async"
+            data-nimg="responsive"
+            style={{
+              position: "absolute",
+              inset: 0,
+              boxSizing: "border-box",
+              padding: 0,
+              border: "none",
+              margin: "auto",
+              display: "block",
+              width: 0,
+              height: 0,
+              minWidth: "100%",
+              maxWidth: "100%",
+              minHeight: "100%",
+              maxHeight: "100%",
+              objectFit: "cover"
+            }}
+          />
+        </span>
+      </div>
+      <p
+        className="dist__StyledText-sc-630f0a6f-12 bhMDEK style__SpotlightTagLine-sc-11272830-2 iERVoj"
+        fontWeight="medium"
+      >
+        Bossy
+      </p>
+      <p
+        className="dist__StyledText-sc-630f0a6f-12 hVVQdP style__SpotlightTitle-sc-11272830-3 cSAGwA"
+        fontWeight="medium"
+      >
+        Therapists, Coaches, and Masterminds: Building Your Personal Support
+        Team
+      </p>
+      <p
+        className="dist__StyledText-sc-630f0a6f-12 bSTuQa style__SpotlightDetails-sc-11272830-4 enGwAp"
+        fontWeight="regular"
+      >
+        Today, Tara &amp; Katie debate the age-old question: Do I need a
+        therapist...or an executive coach?
+      </p>
+      <div className="style__SpotlightSponsor-sc-11272830-5 cBQIlA">
+        <p
+          className="dist__StyledText-sc-630f0a6f-12 gyYyqC"
+          fontWeight="bold"
+        ></p>
+      </div>
+    </a>
+  </div>
+</div>
+
+</div>
+  )
+}
+
+export default HomePage
